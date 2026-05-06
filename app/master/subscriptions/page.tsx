7a07307c9d3f33c04b0d0ce6222dd0f22a3970a9
Kaone <ksetae@gmail.com>
2026-05-06 16:54:04 +0200

    style(master): redesign master dashboard and add placeholder admin pages

---
+++ app/master/subscriptions/page.tsx
@@ -0,0 +1,8 @@
+export default function MasterSubscriptionsPage() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial, sans-serif" }}>
+      <h1 style={{ color: "#0f766e" }}>Subscriptions</h1>
+      <p>Subscription management will be built here.</p>
+    </main>
+  );
+}
---
+++ app/master/users/page.tsx
@@ -0,0 +1,8 @@
+export default function MasterUsersPage() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial, sans-serif" }}>
+      <h1 style={{ color: "#0f766e" }}>Users</h1>
+      <p>User access management will be built here.</p>
+    </main>
+  );
+}
